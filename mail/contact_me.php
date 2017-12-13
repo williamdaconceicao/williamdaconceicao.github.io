a5d669ba7699b1445e1fff3f6b7d5e02e7054bc0
willidaconceicao <willifbi0077@gmail.com>
2017-12-13 11:03:36 +0100

    Initialisation du portefolio

---
+++ mail/contact_me.php
@@ -0,0 +1,22 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = '223689@supinfo.com';
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+mail($to,$email_subject,$email_body);
+return true;			
+?>
